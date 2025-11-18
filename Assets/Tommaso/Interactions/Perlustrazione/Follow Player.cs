60ffa18c93a2e06503d374c4e4f12408cce96116
Tommaso-G <73938155+Tommaso-G@users.noreply.github.com>
2025-11-18 14:24:56 +0100

    Perlustrazione di tutte le aule, ancora da capire come fare alcune cose

---
+++ Assets/Tommaso/Interactions/Perlustrazione/Follow Player.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowerAgent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+    private bool isFollowing = false;
+
+    public Animator MyAnimator;
+
+    public Collider destinationCollider;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+            agent = gameObject.AddComponent<NavMeshAgent>();
+
+        
+    }
+
+    void OnEnable()
+    {
+        FollowPlayerBehavior.OnFollowPlayerTriggered += HandleFollowTriggered;
+    
+    }
+
+    void OnDisable()
+    {
+        FollowPlayerBehavior.OnFollowPlayerTriggered -= HandleFollowTriggered;
+    }
+
+    private void HandleFollowTriggered(GameObject follower, GameObject player)
+    {
+        // ✅ Solo se l'evento riguarda *questo* oggetto
+        if (follower == gameObject)
+        {
+            Debug.Log($"{name} ha ricevuto l'evento di follow verso {player.name}");
+            playerTransform = player.transform;
+            isFollowing = true;
+            MyAnimator.SetBool("GoToRun", true);
+        }
+    }
+
+
+    void Update()
+    {
+        if (isFollowing && playerTransform != null)
+        {
+            agent.SetDestination(playerTransform.position);
+        }
+        
+        detectDestination(destinationCollider);
+
+    }
+
+    private void detectDestination(Collider collider) {
+
+        if (collider.bounds.Contains(transform.position))
+        {
+            agent.isStopped = true; 
+        }
+     }
+
+
+
+
+}
